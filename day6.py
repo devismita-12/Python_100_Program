5aefa0114f0718955965a4eadb1491378c0eb731
DEVI <DEVI123@gmail.com>
2023-07-07 20:32:09 +0530

    swap two variables

---
+++ day6.py
@@ -0,0 +1,8 @@
+#WRITE A PYTHON PROGRAM TO SWAP TWO VARIABLES
+
+a=int(input("Enter the value of a :"))
+b=int(input("Enter the value of b :"))
+a,b=b,a
+
+print("The value of a is ",a)
+print("The value of b is ",b)
