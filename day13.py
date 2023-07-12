37c4bb15af7bbb74d92db82cbfa32711d6e90397
DEVI <DEVI123@gmail.com>
2023-07-12 12:11:25 +0530

    add files

---
+++ day13.py
@@ -0,0 +1,18 @@
+# Check Prime Number
+
+num=int(input("Enter a number :"))
+
+if num<1:
+    print("It is not a nummber :")
+elif num>1:
+    for i in range (2,num):
+        if (num%i ==0):
+            print("Not a prime number")
+            break
+        else:
+            print(" prime number")
+            break
+
+
+
+
