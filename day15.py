dbd1cac76fdfa98faf55d1bad28cfa71e537b41d
DEVI <DEVI123@gmail.com>
2023-08-25 09:50:41 +0530

    It is day 15

---
+++ day15.py
@@ -0,0 +1,13 @@
+# Find the Factorial of a Number
+
+def factorial(n):
+    if n==0 or n==1:
+        return 1
+    else:
+        return  ((n) * factorial(n-1))
+num=int(input("Enter a number :"))
+result=factorial(num)
+print("The factorial of",num,"is=",result)
+
+
+ 
